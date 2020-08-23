070ece7fc323897ad164cb9d3e5a0dc20852c6c0
IJumpAround <ventrn09@gmail.com>
2020-08-23 11:43:56 -0400

    QuestionDetailView tests

--- aftermath_django/polls/tests.py
+++ aftermath_django/polls/tests.py
@@ -82,3 +82,18 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         actual = response.context[self.latest_key]
         self.assertQuerysetEqual(actual, [repr(past1),repr(past2)])
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question("Future question.", 3)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question("Past question.", -4)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
