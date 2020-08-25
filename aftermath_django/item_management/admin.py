a045621c861b86399f5e9d91cf058548535c328b
IJumpAround <ventrn09@gmail.com>
2020-08-24 23:28:11 -0400

    item_management admin

--- aftermath_django/item_management/admin.py
+++ aftermath_django/item_management/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player, Weapon, WeaponTrait, Armor, ArmorTrait, ItemSlot, Rarity, Description, Tier
+
+
+# class ArmorAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']})
+#     ]
+#
+#
+# # class ChoiceAdmin(admin.ModelAdmin):
+# #     fieldsets = [
+# #         ()
+# #     ]
+
+# admin.site.register(Question, QuestionAdmin)
+admin.site.register(Player)
+admin.site.register(Armor)
