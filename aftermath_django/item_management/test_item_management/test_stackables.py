d18622ee9b0c410c35b7ce200297ecc8de29f05c
IJumpAround <ventrn09@gmail.com>
2020-10-01 20:58:08 -0400

    Placeholder tests

--- aftermath_django/item_management/test_item_management/test_stackables.py
+++ aftermath_django/item_management/test_item_management/test_stackables.py
@@ -18,8 +18,20 @@ class StackableModelTest(TestCase):
         loose_potions = Stackable.objects.create(stackable_type='potion', name='Potion of Healing', quantity=15)
 
         ref = loose_potions.id
-        new_stack = loose_potions.split(player_name=player.name, amount=3)
+        new_stack = loose_potions.transfer(player_name=player.name, amount=3)
 
         self.assertEqual(new_stack.quantity, 3)
         self.assertEqual(loose_potions.quantity, 12)
         self.assertEqual(ref, loose_potions.id)
+
+    def test_transfer_to_unclaimed(self):
+        raise NotImplemented
+
+    def test_split_to_existing_stack_combines(self):
+        raise NotImplemented
+
+    def test_transfer_more_than_available(self):
+        raise NotImplemented
+
+    def test_transfer_all(self):
+        raise NotImplemented
